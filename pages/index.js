8022b108d3b6ba026bf7ab73f6e4a5eff9afb9c2
AGMarasco <alexmarasco13@gmail.com>
2022-07-17 18:57:21 -0400

    photo image of me, transition upwards

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,11 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import CardMedia from '@mui/material/CardMedia';
 import Slide from '@mui/material/Slide';
 import { styled } from '@mui/material/styles';
+
+import { useRef } from 'react';
 
 import Head from 'next/head'
 import Image from 'next/image'
@@ -18,21 +21,34 @@ const PaperItem = styled(Paper)(({ theme }) => ({
 const Home = () => {
     const context = useAppContext();
 
+    const containerRef = useRef(null);
+
     return (
-        <PaperItem elevation={0} sx={{ height: '100vh' }}>
+        <PaperItem 
+            elevation={0} 
+            sx={{ 
+                height: '100vh'
+            }}
+        >
             <Head>
                 <title>Alexander Marasco</title>
                 <meta name="description" content="Welcome to the personal website for Alexander Marasco" />
                 {/* <link rel="icon" href="/favicon.ico" /> */}
             </Head>
-            <Stack direction="row" sx={{ height: '100%' }}>
+            <Stack 
+                direction="column" 
+                sx={{ 
+                    height: '100%',
+                    zIndex: 2 
+                }}
+            >
                 {/* Name plate at the top */}
                 <Box
                     sx={{
                         width: '100%',
                         display: 'flex',
                         justifyContent: 'center',
-                        mt: '2%'
+                        mt: '3%'
                     }}
                 >
                     <Typography variant='h2' fontWeight={400}>
@@ -40,6 +56,33 @@ const Home = () => {
                     </Typography>
                 </Box>
                 {/* Image */}
+                <Box
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        width: '100%',
+                        overflow: 'hidden',
+                        mt: '2%'
+                    }}
+                >
+                    <Slide 
+                        direction='up' 
+                        in 
+                        mountOnEnter 
+                        unmountOnExit 
+                        container={containerRef.current}
+                        easing='ease-in-out'
+                    >
+                    <CardMedia
+                        component='img'
+                        sx={{
+                            width: 'auto',
+                            maxHeight: '90%'
+                        }}
+                        src='/Alex_Marasco.jpg'
+                    />
+                    </Slide>
+                </Box>
             </Stack>
         </PaperItem>
     )
