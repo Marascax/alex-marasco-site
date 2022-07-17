d60289de8b96e58c41b35beb597cb05f6a0c0e3e
AGMarasco <alexmarasco13@gmail.com>
2022-07-17 16:42:47 -0400

    init commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,41 @@
-import '../styles/globals.css'
+import { AppWrapper } from '../lib/appContext';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const theme = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: '#222831',
+        },
+        secondary: {
+            main: '#00ADB5',
+        },
+        background: {
+            menu: '#222831'
+        }
+    },
+    typography: {
+        fontFamily: '"Roboto", "Ariel", "Helvetica", sans serif'
+    },
+    components: {
+        MuiTextField: {
+            defaultProps: {
+                color: 'secondary',
+            }
+        }
+    }
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <AppWrapper>
+                <Component {...pageProps} />
+            </AppWrapper>
+        </ThemeProvider>
+  )
 }
 
 export default MyApp
